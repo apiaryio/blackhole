872230857f884ff1ec9c25205a5dad8c36e1da90
Ladislav Prskavec <ladislav@apiary.io>
2017-03-02 15:01:03 +0100

    test: add test

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 )
 
 const (
-	CONN_TYPE = "tcp"
-	MESSAGE   = "HTTP/1.1 429 Too Many Requests\r\nConnection: close\r\nContent-Length: 226\r\nContent-Type: text/plain\r\n\r\nWe have detected exceedingly high usage of your mock/proxy server, you are rate limited for the time being. Please reach us at support@apiary.io if you need any help restoring access and please help us understand your use case"
+	MESSAGE = "HTTP/1.1 429 Too Many Requests\r\nConnection: close\r\nContent-Length: 227\r\nContent-Type: text/plain\r\n\r\nWe have detected exceedingly high usage of your mock/proxy server, you are rate limited for the time being. Please reach us at support@apiary.io if you need any help restoring access and please help us understand your use case."
 )
 
-func main() {
-	l, err := net.Listen(CONN_TYPE, ":"+os.Getenv("PORT"))
+func SetupServer() {
+	l, err := net.Listen("tcp", ":"+os.Getenv("PORT"))
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 		os.Exit(1)
@@ -28,4 +27,8 @@ func main() {
 		conn.Write([]byte(MESSAGE))
 		conn.Close()
 	}
+}
+
+func main() {
+	SetupServer()
 }
